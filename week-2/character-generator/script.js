c62e86bfc669f04f5b8a2df4c7dec8464a9fa813
racwhi <128566218+racwhi@users.noreply.github.com>
2024-11-03 21:30:44 -0600

    week 2 character generator

--- week-2/character-generator/script.js
+++ week-2/character-generator/script.js
@@ -3,23 +3,46 @@
   Chapter 1
   Programming Assignment
 
-  Author:
-  Date:
-  Filename:
+  Author: Rachel White
+  Date:11/3/2024
+  Filename: script.js
 */
 
 "use strict";
 
-function createCharacter(name, gender, characterClass) {
+function createCharacter(name, gender, charClass) {
   // TODO: Implement this function
+
+  return {
+    getName: function () {
+      return name;
+    },
+    getGender: function () {
+      return gender;
+    },
+    getClass: function () {
+      return charClass;
+    },
+  };
 }
 
-document.getElementById("generateHero").addEventListener("click", function(e) {
+document.getElementById("generateHero").addEventListener("click", function (e) {
   e.preventDefault();
 
   // TODO: Get form values
+  const name = document.getElementById("heroName").value;
+  const gender = document.getElementById("heroGender").value;
+  const charClass = document.getElementById("heroClass").value;
 
   // TODO: Create character
+  const newCharacter = createCharacter(name, gender, charClass);
 
   // TODO: Display character information
+  const characterOutput = document.getElementById("characterOutput");
+  characterOutput.innerHTML = `
+        <h2>Character Created</h2>
+        <p>Name: ${newCharacter.getName()}</p>
+        <p>Gender:${newCharacter.getGender()}</p>
+        <p>Class: ${newCharacter.getClass()}</p>
+    `;
 });
